fdff4d6d4bfa1ea94971b6322f4dad41a7c0d047
Douglas de Jesus Costa <douglas.jesuscosta@gmail.com>
2018-11-10 10:15:17 -0200

    Adicionando informações a modelagem do cliente

--- src/app/models/cliente.ts
+++ src/app/models/cliente.ts
@@ -3,6 +3,16 @@ import { Identificador } from "./identificador";
 class Cliente {
     constructor(
         private identificadores: Identificador[],
-        
+
     ){}
+
+    public get p_identificadores(): Identificador[] {
+        return this.identificadores;
+    }
+
+    public set p_identificadores(identificadores: Identificador[]){
+        this.identificadores = identificadores;
+    }
+
+    }
 }
---
+++ src/app/models/nome.ts
@@ -0,0 +1,52 @@
+export class NomesCliente{
+
+    constructor(
+        private nomesAtribuidos?: string[],
+        private sobrenomes?: string[],
+        private sufixos?: string[],
+        private indicadorUso?: number,
+        private nomeAlternativo?: string 
+    ){
+
+    }
+
+    public get p_nomesAtribuidos(): string[] {
+        return this.nomesAtribuidos;
+    }
+
+    public set p_nomesAtribuidos(nomesAtribuidos: string[]){
+        this.nomesAtribuidos = nomesAtribuidos;
+    }
+
+    public get p_sobrenomes(): string[]{
+        return this.sobrenomes;
+    }
+
+    public set p_sobrenomes(sobrenomes: string[]){
+        this.sobrenomes = sobrenomes;
+    }
+
+    public get p_sufixos(): string[] {
+        return this.sufixos;
+    }
+
+    public set p_sufixos(sufixos: string[]){
+        this.sufixos = sufixos;
+    }
+
+    public get p_indicadorUso(): number {
+        return this.indicadorUso;
+    }
+
+    public set p_indicadorUso(indicadorUso: number){
+        this.indicadorUso = indicadorUso;
+    }
+
+    public get p_nomeAlternativo(): string {
+        return this.nomeAlternativo;
+    }
+
+    public set p_nomeAlternativo(nomeAlternativo: string){
+        this.nomeAlternativo = nomeAlternativo;
+    }
+}
